c69724db3de7dd5083d660db157bf4e4e62182dc
Herlangga Maulani <maulani.morningstar@gmail.com>
2023-03-12 18:02:13 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div>
+      <div>
+        <h1 className="font-serat">monst</h1>
+        <h1 className="font-crimsonPro">crimson</h1>
+      </div>
+    </div>
+  );
+};
+
+export default App;
