5286bc2efe134d490ccd8aa4d58e71258f4df61d
Herlangga Maulani <maulani.morningstar@gmail.com>
2023-03-13 19:35:07 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useEffect } from "react";
 import { Link } from "react-scroll";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import homepage from "./assets/images/homepage.png";
 import logo from "./assets/images/logo.png";
 import bulan from "./assets/images/bulan.png";
@@ -35,6 +37,10 @@ const App = () => {
   }
 
   useEffect(() => {
+    AOS.init({
+      duration: 500
+    });
+    AOS.refresh();
     const intervalId = setInterval(() => {
       setRemainingTime(calculateRemainingTime());
     }, 1000);
@@ -42,7 +48,7 @@ const App = () => {
   }, []);
 
   return (
-    <div className="lg:w-[700px] lg:mx-auto relative">
+    <div className="lg:w-[600px] lg:mx-auto relative">
       <section
         className="bg-cover"
         style={{ backgroundImage: `url(${homepage})` }}
@@ -52,53 +58,45 @@ const App = () => {
           <div className="pt-28">
             <img src={logo} alt="Logo" className="mx-auto" />
             <img src={bulan} alt="Bulan" className="mx-auto" />
-            <h1 className="font-crimsonPro text-center text-white mt-12 font-bold text-xl">
+            <h1 className="lg:text-3xl font-crimsonPro text-center text-white mt-12 font-bold text-xl">
               “The memories that we never forget”
             </h1>
-            <div className="fixed bottom-[35px] shadow-md opacity-80 w-[290px] rounded-[30px] left-0 right-0 flex mx-auto bg-biruGw py-4 px-6 justify-around items-center space-x-3">
+            <div className="fixed flex bg-biruGw mx-auto left-0 right-0 w-[290px] z-[1000] justify-center space-x-3 p-3 bottom-10 rounded-[30px] opacity-80">
               <Link
                 to="home"
-                spy={true}
                 smooth={true}
-                offset={-70}
-                duration={500}
+                duration={300}
                 className="cursor-pointer"
               >
-                <img src={home} alt="home" className="mx-auto" />
-                <h1>Beranda</h1>
+                <img src={home} alt="home" className="mx-auto lg:w-30" />
+                <h1 className="lg:text-2xl">Beranda</h1>
               </Link>
               <Link
                 to="tanggal"
-                spy={true}
                 smooth={true}
-                offset={-70}
-                duration={500}
+                duration={300}
                 className="cursor-pointer"
               >
-                <img src={kalender} alt="home" className="mx-auto" />
-                <h1>Tanggal</h1>
+                <img src={kalender} alt="home" className="mx-auto lg:w-30" />
+                <h1 className="lg:text-2xl">Tanggal</h1>
               </Link>
               <Link
                 to="waktu"
-                spy={true}
                 smooth={true}
-                offset={-70}
-                duration={500}
+                duration={300}
                 className="cursor-pointer"
               >
-                <img src={jam} alt="home" className="mx-auto" />
-                <h1>Waktu</h1>
+                <img src={jam} alt="home" className="mx-auto lg:w-30" />
+                <h1 className="lg:text-2xl">Waktu</h1>
               </Link>
               <Link
                 to="penting"
-                spy={true}
                 smooth={true}
-                offset={-70}
-                duration={500}
+                duration={300}
                 className="cursor-pointer"
               >
-                <img src={tanda} alt="home" className="mx-auto" />
-                <h1>Penting</h1>
+                <img src={tanda} alt="home" className="mx-auto lg:w-30" />
+                <h1 className="lg:text-2xl">Penting</h1>
               </Link>
             </div>
           </div>
@@ -107,14 +105,14 @@ const App = () => {
       <div className="bg-cover" style={{ backgroundImage: `url(${section2})` }}>
         <div className="h-screen px-[37px]">
           <div className="pt-[360px]">
-            <p className="text-white font-serat font-bold text-center text-lg pl-4">
+            <p data-aos="fade-up" className="text-white font-serat font-bold text-center text-lg pl-4 lg:text-3xl">
               {" "}
               Altansa terinspirasi dari kata yang berasal dari bahasa Arab yaitu
               la-tansa yang berarti ‘jangan lupa’, dengan harapan adanya acara
               ini bisa menjadi sarana kita menjalin silahturahmi satu sama lain
               agar tidak saling melupakan.
             </p>
-            <h1 className="text-white font-serat text-2xl font-bold text-center mt-6">
+            <h1 data-aos="fade-up" className="lg:text-5xl text-white font-serat text-2xl font-bold text-center mt-6">
               -MPK 45
             </h1>
           </div>
@@ -126,26 +124,27 @@ const App = () => {
         id="tanggal"
       >
         <div className="h-screen font-serat">
-          <div className="flex items-center w-[350px] mx-auto text-white justify-center pt-52">
-            <h1 className="text-lg text-center font-bold">9 April 2023</h1>
+          <div className="flex items-center w-[350px] lg:w-[450px] mx-auto text-white justify-center pt-52">
+            <h1 data-aos="fade-right" className="text-lg text-center font-bold lg:text-3xl">9 April 2023</h1>
             <div className="w-[5px] h-16 bg-white rounded-md mx-3"></div>
             <div>
-              <h1 className="text-lg text-center font-bold">16.00 - 19.00</h1>
-              <p className="text-lg font-bold text-center">WIB</p>
+              <h1 data-aos="fade-left" className="text-lg text-center font-bold lg:text-3xl">16.00 - 19.00</h1>
+              <p data-aos="fade-up" className="text-lg font-bold text-center lg:text-3xl">WIB</p>
             </div>
           </div>
           <div className="mt-6">
-            <h1 className="text-center text-white font-bold text-lg">
+            <h1 data-aos="zoom-in" className="text-center text-white font-bold text-lg lg:text-3xl">
               Wingz O Wingz
             </h1>
-            <p className="w-[350px] my-4 mx-auto text-center text-white font-bold text-lg">
+            <p data-aos="zoom-in" className="w-[350px] my-4 mx-auto text-center text-white font-bold text-lg lg:text-3xl">
               Jl. Supratman No.45a, Cihapit, Kec. Bandung Wetan, Kota Bandung,
               Jawa Barat 40114
             </p>
-            <img src={map} alt="map" className="mx-auto" />
+            <img data-aos="fade-down" src={map} alt="map" className="mx-auto lg:w-96" />
             <a
+              data-aos="fade-up"
               href="https://goo.gl/maps/ELisLfy8sNJrRhDs7"
-              className="text-xl text-white shadow-md p-3 rounded-[25px] bg-biruGw block w-[160px] text-center mt-6 mx-auto"
+              className="text-xl text-white shadow-md p-3 lg:py-3 lg:px-6 rounded-[25px] bg-biruTeuing block lg:w-[300px] lg:text-3xl w-[160px] text-center mt-6 mx-auto"
               target="_blank"
             >
               lihat lokasi
@@ -160,27 +159,27 @@ const App = () => {
       >
         <div className="h-screen text-white">
           <div className="font-crimsonPro pt-[350px]">
-            <h1 className="text-3xl w-[190px] mx-auto text-center font-bold">
+            <h1 data-aos="fade-down" className="text-3xl w-[190px] lg:w-auto mx-auto text-center font-bold lg:text-6xl">
               Acara akan diselenggarakan
             </h1>
             <div className="flex justify-center items-start my-4">
-              <h1 className="text-6xl font-bold text-center">
+              <h1 data-aos="zoom-in" className="text-6xl font-bold text-center">
                 {remainingTime.days}
               </h1>
-              <p className="text-2xl ml-1 font-bold">Hari</p>
+              <p data-aos="zoom-in" className="text-2xl ml-1 font-bold">Hari</p>
             </div>
             <div className="flex justify-center space-x-16 text-center">
               <div>
-                <h1 className="text-6xl font-bold">{remainingTime.hours}</h1>
-                <h1 className="text-xl font-bold">Jam</h1>
+                <h1 data-aos="zoom-in" className="text-6xl font-bold">{remainingTime.hours}</h1>
+                <h1 data-aos="zoom-in" className="text-xl font-bold lg:text-5xl">Jam</h1>
               </div>
               <div>
-                <h1 className="text-6xl font-bold">{remainingTime.minutes}</h1>
-                <h1 className="text-xl font-bold">Menit</h1>
+                <h1 data-aos="zoom-in" className="text-6xl font-bold">{remainingTime.minutes}</h1>
+                <h1 data-aos="zoom-in" className="text-xl font-bold lg:text-5xl">Menit</h1>
               </div>
               <div>
-                <h1 className="text-6xl font-bold">{remainingTime.seconds}</h1>
-                <h1 className="text-xl font-bold">Detik</h1>
+                <h1 data-aos="zoom-in" className="text-6xl font-bold">{remainingTime.seconds}</h1>
+                <h1 data-aos="zoom-in" className="text-xl font-bold lg:text-5xl">Detik</h1>
               </div>
             </div>
           </div>
@@ -193,12 +192,13 @@ const App = () => {
       >
         <div className="h-screen text-white font-crimsonPro">
           <div className="pt-96">
-            <h1 className="text-3xl w-[200px] mx-auto text-center font-bold">
+            <h1 data-aos="fade-down" className="text-3xl w-[200px] mx-auto lg:text-6xl lg:w-auto lg:px-10 text-center font-bold">
               Pengisian data kehadiran
             </h1>
             <a
+data-aos="fade-up"
               href="https://forms.gle/okZpkdFsxbXpD5B16"
-              className="font-bold block w-[150px] mx-auto rounded-[25px] py-1 text-2xl mt-4 shadow-md text-center bg-biruTeuing"
+              className="font-bold block w-[150px] mx-auto rounded-[25px] lg:w-[250px] lg:text-5xl lg:py-3 lg:mt-10 py-1 text-2xl mt-4 shadow-md text-center bg-biruTeuing"
             >
               isi data
             </a>
@@ -208,18 +208,18 @@ const App = () => {
       <div className="bg-cover" style={{ backgroundImage: `url(${section6})` }}>
         <div className="h-screen">
           <div className="pt-80">
-            <h1 className="text-4xl font-bold text-white text-center w-[200px] mx-auto mb-8">
+            <h1 data-aos="fade-down" className="text-4xl font-bold text-white text-center w-[200px] mx-auto mb-8 lg:text-6xl">
               Contact Person
             </h1>
             <div className="flex justify-center space-x-6">
               <a href="">
-                <img src={ig} alt="" width={70} />
+                <img data-aos="fade-right" src={ig} alt="" width={70} className="lg:w-32"/>
               </a>
               <a href="">
-                <img src={wa} alt="" width={70} />
+                <img data-aos="zoom-in" src={wa} alt="" width={70} className="lg:w-32"/>
               </a>
               <a href="">
-                <img src={line} alt="" width={70} />
+                <img data-aos="fade-left" src={line} alt="" width={70} className="lg:w-32"/>
               </a>
             </div>
           </div>
